d1bc96bcddcb88a323cfe959f2f85d115e68f332
bouare12 <bandiougoubouare@yahoo.fr>
2016-10-06 00:31:52 +0200

    coms

--- scripts/bonj.js
+++ scripts/bonj.js
@@ -1,2 +1,9 @@
-
-(adsbygoogle = window.adsbygoogle || []).push({});
+jQuery(document).ready(function() {
+        jQuery(".toggle-cat-list").on("click", function(e) {
+            e.preventDefault();
+            jQuery(".cat-list-menu").slideToggle(600);
+        })
+        jQuery(".toggle-cat-list").on("blur", function() {
+            jQuery(".cat-list-menu").slideUp(600);
+        })
+    });
